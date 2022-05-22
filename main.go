89c876eede0529b048370be142db8365bf064e2a
llama <progllama.rust@gmail.com>
2022-05-23 02:10:42 +0900

    [Add] simple query.

--- main.go
+++ main.go
@@ -21,7 +21,14 @@ var (
 	dsn    = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 )
 
+var (
+	id   int
+	name string
+)
+
 func main() {
+	log.SetFlags(log.Llongfile)
+
 	db, err := sql.Open(driver, dsn)
 	if err != nil {
 		log.Fatal(err)
@@ -32,6 +39,24 @@ func main() {
 		fmt.Println("Succeed to connect")
 	} else {
 		log.Fatal("Fail to connect")
+	}
+
+	rows, err := db.Query("select id, name from users where id = ?", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
